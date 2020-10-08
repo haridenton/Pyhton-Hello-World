33ba1457b5e58d5ccf715e0c57fa31da5e765409
Hari Denton <haridenton@yahoo.com>
2020-10-08 07:07:37 +0100

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,4 @@
+# This is a comment, it will not run
+# Delete the # symbol on line 4 to turn the comment into code that will run
+# Edit the text inside the speech marks to say hello world
+#print("BOB")
